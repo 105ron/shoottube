b401f5b488762762733f34ed371c2a1cb3f5fca9
Rhys <rbrooker105@gmail.com>
2020-10-11 10:41:29 +1100

    setup form for upload

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+      this.state = {
+        selectedFile: null
+      }
+  }
+
+  onChangeHandler = event => {
+    this.setState({
+      selectedFile: event.target.files[0],
+      loaded: 0,
+    })
+  }
+
+  onClickHandler = () => {
+    const data = new FormData() 
+    data.append('file', this.state.selectedFile)
+  }
+
+  render() {
+    const { selectedFile } = this.state;
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6 card shadow p-3 mb-5 bg-white rounded" style={ {margin: '30px auto'} }>
+            <form method="post" action="#" id="#">
+              <div className="form-group files" style={ {textAlign: 'center'} }>
+                <label htmlFor="fileupload"style={ {display: 'block'} }>
+                  Upload Your File
+                </label>
+                  <input id="fileupload" type="file" name="file" onChange={this.onChangeHandler} />
+                  <button
+                    className="btn btn-success btn-block"
+                    disabled={!selectedFile}
+                    onClick={this.onClickHandler}
+                    style={ {margin: '10px auto', width: '50%'} }
+                    type="button"
+                  >
+                    Upload
+                  </button> 
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
